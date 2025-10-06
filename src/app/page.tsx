8d17261eb30aafdd8cd4356937ae7084a301c599
Thirawat74 <sawn4568@gmail.com>
2025-10-06 19:23:53 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       <div>
         <p className="text-5xl sm:text-7xl">ยินดีต้อนรับเข้าสู่</p>
         <span className="text-3xl sm:text-5xl font-bold bg-gradient-to-r from-blue-500 to-white-500 text-transparent bg-clip-text">
-          Nadia Store
+          NADIA STORE
         </span>
         <div className="flex justify-center space-x-4 mt-6">
           <AlertDialog>
